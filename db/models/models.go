b2f772b3666704e8d2acf69d858781eeb9cfd31c
3dw1nM0535 <workockmoses@gmail.com>
2020-03-02 20:50:06 +0300

    ft(refactor database model and GraphQL schema)

--- db/models/models.go
+++ db/models/models.go
@@ -3,7 +3,6 @@ package models
 import (
 	"time"
 
-	"github.com/lib/pq"
 	"github.com/satori/go.uuid"
 )
 
@@ -11,15 +10,11 @@ type BaseModel struct {
 	ID        uuid.UUID `gorm:"primary_key;type:uuid;default:uuid_generate_v4()"`
 	CreatedAt time.Time `gorm:"default:CURRENT_TIMESTAMP"`
 	UpdatedAt time.Time
-	DeletedAt time.Time
 }
 
 type Deli struct {
 	BaseModel
-	RestaurantName string         `gorm:"not_null;type:varchar(100)"`
-	Telephone      string         `gorm:"not_null;type:varchar(50)"`
-	Delicacies     pq.StringArray `gorm:"type:varchar(100)[]"`
-	Verified       bool           `gorm:"default:false;not_null"`
-	Rating         float64        `gorm:"default:0;not_null"`
-	Reviews        pq.StringArray `gorm:"type:varchar(100)[]"`
+	RestaurantName string `gorm:"not_null;type:varchar(100)"`
+	Telephone      string `gorm:"not_null;type:varchar(50)"`
+	Verified       bool   `gorm:"default:false;not_null"`
 }
