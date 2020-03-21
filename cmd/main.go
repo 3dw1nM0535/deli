f583c24192cc13b6f96d19677678b910d8aedfce
3dw1nM0535 <workockmoses@gmail.com>
2020-03-21 14:48:50 +0300

    ch(code refactor)

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,9 @@
 package main
 
 import (
-	"fmt"
-
-	"github.com/3dw1nM0535/deli/db"
 	"github.com/3dw1nM0535/deli/server"
 )
 
 func main() {
-	orm, err := db.Factory()
-	if err != nil {
-		fmt.Errorf("Error connecting to database: %v", err.Error())
-	}
-	server.Run(orm)
+	server.Run()
 }
