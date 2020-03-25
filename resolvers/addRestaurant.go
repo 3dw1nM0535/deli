ca6ebcc06a76bfb7b1f085a491b50bb35aab48d3
3dw1nM0535 <workockmoses@gmail.com>
2020-03-25 18:53:25 +0300

    ft(Deli):input validation

--- resolvers/addRestaurant.go
+++ resolvers/addRestaurant.go
@@ -2,12 +2,25 @@ package resolvers
 
 import (
 	"context"
+	"errors"
 
 	models1 "github.com/3dw1nM0535/deli/db/models"
 	"github.com/3dw1nM0535/deli/models"
 )
 
 func (r *mutationResolver) AddRestaurant(ctx context.Context, input models.RestaurantInput) (*models1.Restaurant, error) {
+	if input.RestaurantName == "" {
+		err := errors.New("restaurant name cannot be empty")
+		return &models1.Restaurant{}, err
+	}
+	if input.About == "" {
+		err := errors.New("restaurant breif description cannot bt empty")
+		return &models1.Restaurant{}, err
+	}
+	if input.Telephone == "" {
+		err := errors.New("restaurant contact information cannot be empty")
+		return &models1.Restaurant{}, err
+	}
 	var newRestaurant = &models1.Restaurant{
 		RestaurantName: input.RestaurantName,
 		About:          input.About,
