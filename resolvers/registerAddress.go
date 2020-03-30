e2c66ea87ad7e71ca4b0310a269f1f006bd726ce
3dw1nM0535 <workockmoses@gmail.com>
2020-03-30 20:43:24 +0300

    ft(Deli):validate restaurantID before attaching address

--- resolvers/registerAddress.go
+++ resolvers/registerAddress.go
@@ -16,6 +16,10 @@ func (r *mutationResolver) RegisterAddress(ctx context.Context, input models.Add
 	if addr.PostalTown != "" {
 		var restaurant = &models1.Restaurant{}
 		r.ORM.DB.First(&restaurant, "id = ?", input.RestaurantID)
+		if restaurant.ID.String() == "00000000-0000-0000-0000-000000000000" {
+			err := fmt.Errorf("no restaurant with id '%s' to attach address to", input.RestaurantID)
+			return &models1.Address{}, err
+		}
 		var restaurants = []*models1.Restaurant{restaurant}
 		var address = &models1.Address{
 			PostalCode:   input.PostalCode,
