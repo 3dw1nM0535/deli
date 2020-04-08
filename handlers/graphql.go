e4d352536fb4c4f8596933767b192a1f692f0fcb
3dw1nM0535 <workockmoses@gmail.com>
2020-04-08 16:45:42 +0300

    ft(Deli):real-time streaming when making orders

--- handlers/graphql.go
+++ handlers/graphql.go
@@ -1,33 +1,21 @@
 package handler
 
 import (
-	"net/http"
-	"time"
-
 	"github.com/3dw1nM0535/deli/db"
 	graph "github.com/3dw1nM0535/deli/graph/generated"
 	resolver "github.com/3dw1nM0535/deli/resolvers"
 	"github.com/99designs/gqlgen/handler"
+	option "github.com/99designs/gqlgen/handler"
 	"github.com/gin-gonic/gin"
-	"github.com/gorilla/websocket"
 )
 
 // GraphQL : websocket handler
-func GraphQL(orm *db.ORM) gin.HandlerFunc {
+func GraphQL(orm *db.ORM, options []option.Option) gin.HandlerFunc {
 	// Pass Configs
 	cfg := graph.Config{
 		Resolvers: &resolver.Resolver{
 			ORM: orm,
 		},
-	}
-
-	options := []handler.Option{
-		handler.WebsocketKeepAliveDuration(40 * time.Second),
-		handler.WebsocketUpgrader(websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-		}),
 	}
 
 	h := handler.GraphQL(graph.NewExecutableSchema(cfg), options...)
@@ -48,14 +36,6 @@ func GraphQL(orm *db.ORM) gin.HandlerFunc {
 	// srv.Use(extension.Introspection{})
 
 	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-		c.Header("Access-Control-Allow-Headers",
-			"Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-		if c.Request.Method == "OPTIONS" {
-			return
-		}
 		h.ServeHTTP(c.Writer, c.Request)
 	}
 }
--- server/server.go
+++ server/server.go
@@ -3,28 +3,49 @@ package server
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/3dw1nM0535/deli/db"
 	handler "github.com/3dw1nM0535/deli/handlers"
 	"github.com/3dw1nM0535/deli/utils"
+	option "github.com/99designs/gqlgen/handler"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/gorilla/websocket"
 	"github.com/joho/godotenv"
 )
 
 var host, port string
+var options []option.Option
+var c gin.HandlerFunc
 
 func init() {
 	godotenv.Load()
 	host = utils.MustGetEnv("SERVER_HOST")
 	port = utils.MustGetEnv("SERVER_PORT")
+
+	c = cors.Default()
+
+	options = []option.Option{
+		option.WebsocketKeepAliveDuration(10 * time.Second),
+		option.WebsocketUpgrader(websocket.Upgrader{
+			CheckOrigin: func(r *http.Request) bool {
+				return true
+			},
+			ReadBufferSize:  1024,
+			WriteBufferSize: 1024,
+		}),
+	}
 }
 
 // SetupRouter : set routing paths
 func SetupRouter(orm *db.ORM) *gin.Engine {
 	r := gin.Default()
-	r.POST("/query", handler.GraphQL(orm))
+	r.Use(c)
+	r.POST("/query", handler.GraphQL(orm, options))
 	r.GET("/graphql", handler.Playground())
 	r.GET("/", handler.Ping())
+	r.GET("/ws", handler.GraphQL(orm, options))
 	return r
 }
 
