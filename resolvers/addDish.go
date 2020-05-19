3fda875b0c501d6e23bfe6ae520d7a02c00ba112
3dw1nM0535 <workockmoses@gmail.com>
2020-05-19 23:29:34 +0300

    ft(code refactor)

--- resolvers/addDish.go
+++ resolvers/addDish.go
@@ -26,50 +26,6 @@ func menuExists(id string) bool {
 	return true
 }
 
-// func mapItemsToDish(items []*models1.DishInput) ([]*models.Dish, error) {
-// 	ctx := context.Background()
-// 	dishes := []*models.Dish{}
-// 	// validate input for null
-// 	if len(items) == 0 {
-// 		return []*models.Dish{}, errors.New("dishes cannot be empty")
-// 	}
-
-// 	for i := range items {
-// 		if items[i].Title == "" {
-// 			return []*models.Dish{}, errors.New("dish title cannot be empty")
-// 		}
-// 		if items[i].Description == "" {
-// 			return []*models.Dish{}, errors.New("dish description cannot be empty")
-// 		}
-// 		if items[i].Image.Filename == "" {
-// 			return []*models.Dish{}, errors.New("you must provide dish image")
-// 		}
-// 		if fmt.Sprintf("%.2f", float64(items[i].Price)) == "0.00" {
-// 			return []*models.Dish{}, errors.New("dish price must be known to customers")
-// 		}
-// 		if menuExists(items[i].MenuID) == false {
-// 			return []*models.Dish{}, errors.New("dish must belong to a menu. provide a valid menu id")
-// 		}
-
-// 		file := items[i].Image.File
-// 		fileName := items[i].Image.Filename
-// 		_, attr, err := utils.Upload(ctx, file, dishesBucketName, credPath, projectID, fileName)
-// 		if err != nil {
-// 			return []*models.Dish{}, err
-// 		}
-
-// 		d := &models.Dish{
-// 			Title:       items[i].Title,
-// 			Description: items[i].Description,
-// 			Price:       items[i].Price,
-// 			Image:       attr.MediaLink,
-// 			MenuID:      utils.ParseUUID(items[i].MenuID),
-// 		}
-// 		dishes = append(dishes, d)
-// 	}
-// 	return dishes, nil
-// }
-
 func (r *mutationResolver) AddDish(ctx context.Context, input models1.DishInput) (*models.Dish, error) {
 	// validate data
 	if input.MenuID == "" {
