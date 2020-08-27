f4cb9dbeef80e07b61458d17f57fb8235b185ace
3dw1nM0535 <workockmoses@gmail.com>
2020-08-27 14:32:12 +0300

    chore:refactor(booker bookings resolver)

--- resolvers/addBooking.go
+++ resolvers/addBooking.go
@@ -7,13 +7,22 @@ import (
 )
 
 func (r *mutationResolver) AddBooking(ctx context.Context, input *models1.BookingInput) (*models.Booking, error) {
-	newBooking := &models.Booking{
-		Volume:    input.Volume,
-		Booker:    input.Booker,
-		Deposit:   input.Deposit,
-		Token:     input.Token,
-		Delivered: input.Delivered,
+	booking := &models.Booking{}
+	r.ORM.DB.Where("booker = ?", input.Booker).Find(&booking)
+	if booking.ID.String() == specialUUID && r.ORM.DB.NewRecord(booking) {
+		newBooking := &models.Booking{
+			Volume:    input.Volume,
+			Booker:    input.Booker,
+			Deposit:   input.Deposit,
+			Token:     input.Token,
+			Delivered: input.Delivered,
+		}
+		r.ORM.DB.Create(&newBooking)
+		return newBooking, nil
 	}
-	r.ORM.DB.Create(&newBooking)
-	return newBooking, nil
+	booking.Volume = input.Volume
+	booking.Deposit = input.Deposit
+	booking.Delivered = input.Delivered
+	r.ORM.DB.Save(&booking)
+	return booking, nil
 }
